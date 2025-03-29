38e3ec84a3f57f46702369b0ec901d6e0fdd0a5d
Ryan Nahm <rnahm@tjws-06.cse.nd.edu>
2025-03-29 19:25:50 +0000

    working ipfs + spark?

--- ipfs/start-ipfs.sh
+++ ipfs/start-ipfs.sh
@@ -13,15 +13,19 @@ else
   exit 1
 fi
 
+ipfs bootstrap rm --all
+
 # Configure IPFS to listen on all interfaces
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5002
-ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8081
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8082
 # ipfs config Addresses.Swarm /ip4/0.0.0.0/tcp/4001
 # Configure Swarm to listen on all interfaces (use JSON format for array)
 ipfs config Addresses.Swarm --json '[
   "/ip4/0.0.0.0/tcp/4002",
   "/ip6/::/tcp/4002"
 ]'
+
+ipfs bootstrapp /ip4/129.74.152.201/tcp/4002/12D3KooWR3jy9T76xsERNHqNDKSN6f3i82Mzh3bYrgV463xwj42R
 
 ipfs config Routing.Type none
 
