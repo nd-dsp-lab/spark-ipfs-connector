c6760807756b143cc18acccfae90931b361cc7ec
Ryan Nahm <rnahm@tjws-06.cse.nd.edu>
2025-04-07 18:24:42 +0000

    added master ipfs for faster transfer times

--- ipfs/start-ipfs.sh
+++ ipfs/start-ipfs.sh
@@ -10,6 +10,7 @@ if [ ! -f /data/ipfs/config ]; then
     # Reset bootstrap nodes to defaults
     ipfs bootstrap rm --all
     ipfs bootstrap add --default
+    ipfs bootstrap add /ip4/129.74.152.201/tcp/4002/p2p/12D3KooWArcnmCkBTJqVu1iu6uThDF8vjJyeNn54RDmS61WUQVhy
 fi
 
 # Configure IPFS to listen on all interfaces
